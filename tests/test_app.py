c5554878c734172e5b6265164b03bdd38c47ac77
Mads Christian <madscba@gmail.com>
2025-07-11 17:32:03 +0200

    updated w. import sys

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,3 @@
-import sys
-import os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from src.classes.engine import GameEngine
 from src.classes.game_status import GameStatus
