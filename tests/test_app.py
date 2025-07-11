c2ab53e20473ee47a53fcd7bfea79a5cf16ebfb6
Mads Christian <madscba@gmail.com>
2025-07-11 16:21:35 +0200

    added simple test

---
+++ tests/test_app.py
@@ -0,0 +1,9 @@
+from src.classes.engine import GameEngine
+from src.classes.game_status import GameStatus
+
+
+class TestGameEngine():
+    def test_three_move_winning_sequence(self):
+        game_engine = GameEngine(mock_players=True,mock_moves=[[(0,0),(0,1),(0,2)],[(1,0),(1,1),(1,2)]])
+        game_engine.main()
+        assert (game_engine.game_status == GameStatus.FINISHED)
